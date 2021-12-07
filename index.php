c87519199d4caec8fb5e5f70b3bbf14c3456fe0a
Methawi09 <86421053+Methawi09@users.noreply.github.com>
2021-12-07 13:34:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,42 +6,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
 </head>
 <body>
- 
     <table>
         <thead>
             <tr>
                 <th>ID</th><th>First Name</th><th>Last Name</th>
             </tr>
         </thead>
-        <tbody id="tblStudent">
+        <tbody id="tblPosts">
         </tbody>
     </table>
+
+    
 </body>
 <script>
- 
-function loadJSON(){
-    var url = "https://cj-android-demon.herokuapp.com/json2.php";
-    $.getJSON(url)
-        .done((data)=>{
-            console.log(data);
-            $.each(data,  (k, item)=>{
-                console.log(k);
-                console.log(item);
-                var line = "<tr>";
-                    line += "<td>" + (k+1) + "</td>";
-                    line += "<td>" + item.fname + "</td>";
-                    line += "<td>" + item.lname + "</td>";
-                    line += "</tr>";
-                $("#tblStudent").append(line);
+    function loadPosts(){
+        var url = "https://jsonplaceholder.typicode.com/Posts";
+        $.getJSON(url)
+            .done((data)=>{
+                console.log(data);
+                $.each(data,  (k, item)=>{
+                    console.log(item);
+                    var line = "<tr>";
+                        line += "<td>" + item.id + "</td>";
+                        line += "<td>" + item.title + "</td>";
+                        line += "<td>" + item.userid + "</td>";
+                        line += "</tr>";
+                        $("#tblPosts").append(line);
+
+
+                });
+            .fail((xhr, status, err)=>{
             });
-        })
-        .fail((xhr, status, err)=>{
-        });
-}
-$(()=>{
-    loadJSON();
-});
+
+    })
+    }
 </script>
+
 </html>
