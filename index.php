4e399498f30ea1eaa30dd7153cd36dff04ad9b7d
Methawi09 <86421053+Methawi09@users.noreply.github.com>
2021-12-07 10:49:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-Methawi Tantiyavut 63108518
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script></head>
+<body>
+    <button id="btnjson1">json1</button>
+</body>
+<script>
+    function loadjson
+    var url = "https://cj-android-demon.herokuapp.com/json1.php" ;
+    $.get(url, (data,status)=>{
+        console.log(data);
+    })
+
+
+    $(()=>{
+        $("btnjson1").click(loadjson)
+
+    })
+</script>
+
+</html>
