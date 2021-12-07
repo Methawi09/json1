8eb7adeeb875489ce17bac0b4c0c57351ca18717
Methawi09 <86421053+Methawi09@users.noreply.github.com>
2021-12-07 11:45:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
  
@@ -25,27 +23,23 @@
  
 function loadJSON(){
     var url = "https://cj-android-demon.herokuapp.com/json2.php";
-
     $.getJSON(url)
         .done((data)=>{
             console.log(data);
-            $.each(data, (k, item)=>{
+            $.each(data,  (k, item)=>{
                 console.log(k);
                 console.log(item);
                 var line = "<tr>";
-                    line += "<td>"+ (k+1) +"</td>"
-                    line += "<td>"+item.fname+"</td>"
-                    line += "<td>"+item.lname+"</td>"
+                    line += "<td>" + (k+1) + "</td>";
+                    line += "<td>" + item.fname + "</td>";
+                    line += "<td>" + item.lname + "</td>";
                     line += "</tr>";
-                $("#tblDant").append(line);
-
+                $("#tblStudent").append(line);
             });
         })
         .fail((xhr, status, err)=>{
-
         });
 }
-
 $(()=>{
     loadJSON();
 });
