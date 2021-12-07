4f4566721e18c3f405993a698193a71e57f1b939
Methawi09 <86421053+Methawi09@users.noreply.github.com>
2021-12-07 11:00:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,20 @@
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script></head>
 <body>
     <button id="btnjson1">json1</button>
+    Name : <span id="fname"></span> <br/>
+    sName : <span id="lname"></span>
 </body>
 <script>
-    function loadjson
-    var url = "https://cj-android-demon.herokuapp.com/json1.php" ;
-    $.get(url, (data,status)=>{
-        console.log(jData.fname);
-        console.log(jData.lname);
-    })
+    function loadjson(){
+        var url = "https://cj-android-demon.herokuapp.com/json1.php" ;
+        $.get(url, (data,status)=>{
+            console.log(JData.fname);
+            console.log(JData.lname);
+            $("#fname").text(JData.fmane)
+            $("#lname").text(JData.lmane)
+
+        })
+    }
 
 
     $(()=>{
